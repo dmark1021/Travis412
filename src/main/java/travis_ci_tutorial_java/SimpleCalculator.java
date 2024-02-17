0d6e539320c530bf05afb827489a1ee5d3f4a2fd
Henryjones924 <144754010+Henryjones924@users.noreply.github.com>
2024-02-17 16:55:46 -0500

    Update SimpleCalculator.java

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,21 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+
+	  public static int minus (int a, int b)
+	   {
+		   return a-b;
+	   }
+	   
+	   public static int multiple(int a, int b)
+	   {
+		   return a*b;
+	   }
+	   
+	   public static int divide(int a, int b)
+	   {
+		   return a/b;
+	   }
+	
+	
 }
